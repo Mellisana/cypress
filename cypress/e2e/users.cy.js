a85a4c79ae9c15bccbd48bdd089f4c8974c3d926
Эля <elyasalmanovaasr@gmail.com>
2025-06-20 02:18:23 +0700

    Update users.cy.js

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,24 +1,67 @@
-describe('Petstore API Users', () => {
-  let user;
-  let updatedUser;
-  let baseURL;
+describe('Authorization',() => {
+    beforeEach(()=>{
+    cy.viewport(
+        Cypress.config("viewportWidth"),
+        Cypress.config("viewportHeight")
+    );
+      cy.visit('/');
+    })
 
-  beforeEach(() => {
-    cy.fixture('userData.json').then((data) => {
-      user = data.user;
-      updatedUser = data.updatedUser;
-      baseURL = data.baseURL;
+    it("Should open the main page", () => {
+      cy.visit("localhost:3000");
+      cy.contains('Books list');
     });
-  });
 
-  it('updates and reads user', () => {
-    cy.createUser(baseURL, user)
-      .then(() => {
-        cy.updateUser(baseURL, user.username, updatedUser);
-        cy.verifyUser(baseURL, updatedUser.username, updatedUser);
-      })
-      .then(() => {
-        cy.deleteUser(baseURL, updatedUser.username);
-      });
-  });
+    it('log in books list', ()=>{
+        cy.contains('Log in').click();
+        cy.login("bropet@mail.ru", "123");
+        cy.contains("Добро пожаловать").should("be.visible", true);
+    });
+
+    it("Empty login",()=>{
+        cy.contains('Log in').click();
+        cy.login(null, "123");
+        cy.get('#mail').then((elements)=>{
+            expect(elements[0].checkValidity()).to.be.false;
+            expect(elements[0].validationMessage).to.be.eql("Заполните это поле.");
+        });
+    })
+
+    it("Empty password",()=>{
+        cy.contains('Log in').click();
+        cy.login("bropet@mail.ru", null);
+        cy.get('#pass').then((elements)=>{
+            expect(elements[0].checkValidity()).to.be.false;
+            expect(elements[0].validationMessage).to.be.eql("Заполните это поле.");
+        });
+    });
+});
+
+describe('Add book',() => {
+    beforeEach(()=>{
+      cy.visit('/');
+      cy.contains('Log in').click();
+      cy.login("bropet@mail.ru", "123");
+    })
+
+    it("Add new book",()=>{
+        cy.contains('Add new').click();
+        cy.get('#title').type("Буратино");
+        cy.contains("Submit").click();
+    });
+
+    it("Add favorite book",()=>{
+        cy.contains("Add to favorite").click();
+        cy.contains("Favorites").click();
+        cy.get(".card-body").then((elements) => {
+            expect(elements).to.have.length.at.least(1);
+        });
+    });
+
+    it("Delete favorite book",()=>{
+        cy.contains("Favorite").click();
+        cy.contains("Delete from favorite").click();
+        cy.contains("Please add some book").should("be.visible");
+    });
+
 });
